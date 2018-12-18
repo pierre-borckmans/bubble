f2c83352d59be42aed2b012af03c743be1472df5
Pierre Borckmans <pierre.borckmans@letgo.com>
2018-12-18 14:49:51 +0100

    REMOVE unused build script

--- build.sh
+++
@@ -1,6 +0,0 @@
-#rm ../build/app/Bubble/Contents/Resources/app.asar
-#asar pack . ../build/app/Bubble/Contents/Resources/app.asar
-#rm -rf ../build/app/Bubble.app
-#cp -r ../build/app/Bubble ../build/app/Bubble.app
-#rm -rf /Applications/Bubble.app
-cp -rf build/Bubble-darwin-x64/Bubble.app /Applications
